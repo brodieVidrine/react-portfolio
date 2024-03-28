06d72faa5e1f022274d29f09e402ec70bee27205
brodievidrine <bvidrine@sieducation.com>
2024-03-27 23:48:37 -0500

    added svg lotus logo and made started refining the nav bar and content area

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
+import LotusSVG from "../lotus.svg";
 
 function NavBar(){
     return(
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg className="bi me-2" width="40" height="32"></svg>
-        <span className="fs-4">Brodie Vidrine</span>
-      </a>
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-1 border-bottom">
+        <Link to="/" className="d-flex align-items-center mb-3 ms-2 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <img src={LotusSVG} width="40" height="40"></img>
+          <span className="ms-2 fs-4">Brodie Vidrine</span>
+        </Link>
 
       <ul className="nav nav-pills">
         <li className="nav-item"><Link to="/" className="nav-link" aria-current="page">Home</Link></li>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,19 +9,25 @@ import Col from 'react-bootstrap/Col';
 function HomePage(){
     return(
         
-        <Container fluid>
+        <Container fluid className="bgc">
             <NavBar></NavBar>
-            <Row className="w-75 mx-auto">
-                <h1>Home Page</h1>
-                <p>Welcome to the site! There isn't much here yet.</p>
-                <p>The site was built using NodeJS, React, and AWS Amplify. 
+            <Row className="w-75 mx-auto contentArea">
+                <h1 className="w-75 mx-auto align-middle">Welcome!</h1>
+                <p className="w-75 mx-auto">
+                    Thanks for visiting my portfolio!
+                    <br ></br>
+                    <br ></br>
+                    The site was built using NodeJS, React, and AWS Amplify. 
                     It's currently hooked up to MongoDB and S3 buckets over at AWS. 
                     I've set it up to auto-deploy every time the Git repository is updated.
+                    <br ></br>
+                    <br ></br>
+                    There was a GraphQL/MongoDB example set up, but I've temporarily taken it down while I wire up React Router. 
+                    I've got a lot of work I'd like to show off, so organizing with pages will be ideal.
                 </p>
-                <p>There was a GraphQL/MongoDB example set up, but I've temporarily taken it down while I wire up React Router. I've got a lot of work I'd like to show off, so organizing with pages will be ideal.</p>
             </Row>
             
-            </Container>
+        </Container>
     )
 }
 
