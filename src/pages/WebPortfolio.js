dba67c6403d9b74ab9b7de5a249c577a9992c000
brodievidrine <bvidrine@sieducation.com>
2024-03-29 19:13:42 -0500

    added a nice background svg

--- src/pages/WebPortfolio.js
+++ src/pages/WebPortfolio.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import ContentArea from "../components/ContentArea";
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ContentArea from "../components/ContentArea";
+import Card from 'react-bootstrap/Card';
 
 
 function WebPortfolio(){
@@ -14,6 +15,24 @@ function WebPortfolio(){
             <NavBar></NavBar>
             <ContentArea>
                     <h1 className="w-75 mx-auto mt-5">Web Portfolio</h1>
+                    <br></br>
+                    <Row xs={1} md={2} className="g-4">
+                        {Array.from({ length: 4 }).map((_, idx) => (
+                        <Col key={idx}>
+                            <Card>
+                                <Card.Img variant="top" alt="demo thumnail" src="" />
+                                <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit
+                                    longer.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        ))}
+                    </Row>
             </ContentArea>
         </Container>
     )
