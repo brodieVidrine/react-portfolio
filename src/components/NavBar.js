0fcab53a70f0cb12761c887cb2f12d02312fe6a6
brodievidrine <bvidrine@sieducation.com>
2024-03-30 08:28:08 -0500

    working web portfolio loop and card deck

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-import LotusSVG from "../lotus.svg";
+import LotusSVG from "../static/gr/lotus.svg";
 
 function NavBar(){
     return(
       <header className="d-flex flex-wrap justify-content-center py-3 mb-1 border-bottom">
         <Link to="/" className="d-flex align-items-center mb-3 ms-2 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <img src={LotusSVG} width="40" height="40"></img>
+          <img src={LotusSVG} width="40" height="40" ></img>
           <span className="ms-2 fs-4">Brodie Vidrine</span>
         </Link>
 
--- src/pages/WebPortfolio.js
+++ src/pages/WebPortfolio.js
@@ -12,13 +12,34 @@ import Col from 'react-bootstrap/Col';
 
 import bcThumb from "../static/gr/bcThumb.png";
 
-const thumbs = [{
+const thumbs = [
+    {
     id: 0,
     name: 'Base Camp',
-    path: '../static/html/basecamp.html',
-    description: 'I turned an HTML5 recreation of Wolfenstein 3D into a kid-friendly game hub. Many of our customers say its their favorite feature.',
+    path: "/",
+    description: 'I turned an HTML5 recreation of Wolfenstein 3D into a kid-friendly game hub.',
     imageId: bcThumb
-  }];
+  },{
+    id: 0,
+    name: 'Rocket Gal',
+    path: "/",
+    description: 'A minigame designed to help teach kids about visualzing fractions.',
+    imageId: bcThumb
+  },{
+    id: 0,
+    name: 'The Ascend Student Experience',
+    path: "/",
+    description: "This is a static version of our web application that I built for our marketing site. Popular with our sales team.",
+    imageId: bcThumb
+  },{
+    id: 0,
+    name: 'Flash Card Math',
+    path: "/",
+    description: 'This was one of my first projects at Ascend Math. I recreated and improved a legacy Flash teaching aid.',
+    imageId: bcThumb
+  }
+
+];
 
 
 function WebPortfolio(){
@@ -26,11 +47,13 @@ function WebPortfolio(){
     const [fullscreen, setFullscreen] = useState(true);
     const [show, setShow] = useState(false);
     const [modTitle, setModTitle] = useState("");
+    const [demoPath, setDemoPath] = useState("");
 
     function handleShow(title, path) {
         //setFullscreen(breakpoint);
         setShow(true);
         setModTitle(title);
+        
     }
 
     return(
@@ -38,7 +61,7 @@ function WebPortfolio(){
         <Container fluid className="bgc  min-vh-100 d-flex flex-column">
             <NavBar></NavBar>
             <ContentArea>
-                <h1 className="w-75 mx-auto mt-5">Web Portfolio</h1>
+                <h1 className="w-75 mx-auto mt-5 text-center">Web Portfolio</h1>
                 <br></br>
                 <Row xs={1} md={2} className="g-4">
                     {thumbs.map( data =>
@@ -58,7 +81,9 @@ function WebPortfolio(){
                     <Modal.Header closeButton>
                     <Modal.Title>{modTitle}</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>Modal body content</Modal.Body>
+                    <Modal.Body>
+                        
+                    </Modal.Body>
                 </Modal>
             </ContentArea>
         </Container>
