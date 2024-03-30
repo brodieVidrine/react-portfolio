edebcc58353b3322ba07506a5cb755bfd5f6a6cb
brodievidrine <bvidrine@sieducation.com>
2024-03-29 23:25:15 -0500

    got the modal opening on thumbnail click

---
+++ src/components/DemoThumb.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+function DemoThumb({
+    title = "Card Title",
+    img,
+    text,
+    path,
+    callBack
+}){
+
+    function clicked(){
+        callBack(title, path);
+    }
+
+ return (
+    
+    <Card className="demoCard" onClick={clicked}>
+        <Card.Img variant="top" alt="demo thumnail" src={img} />
+        <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+            {text}
+        </Card.Text>
+        </Card.Body>
+    </Card>
+ );   
+}
+
+export default DemoThumb
--- src/pages/WebPortfolio.js
+++ src/pages/WebPortfolio.js
@@ -1,38 +1,65 @@
 import React from "react";
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
 import NavBar from "../components/NavBar";
 import ContentArea from "../components/ContentArea";
+import DemoThumb from "../components/DemoThumb";
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+
+import bcThumb from "../static/gr/bcThumb.png";
+
+const thumbs = [{
+    id: 0,
+    name: 'Base Camp',
+    path: '../static/html/basecamp.html',
+    description: 'I turned an HTML5 recreation of Wolfenstein 3D into a kid-friendly game hub. Many of our customers say its their favorite feature.',
+    imageId: bcThumb
+  }];
 
 
 function WebPortfolio(){
+
+    const [fullscreen, setFullscreen] = useState(true);
+    const [show, setShow] = useState(false);
+    const [modTitle, setModTitle] = useState("");
+
+    function handleShow(title, path) {
+        //setFullscreen(breakpoint);
+        setShow(true);
+        setModTitle(title);
+    }
+
     return(
         
         <Container fluid className="bgc  min-vh-100 d-flex flex-column">
             <NavBar></NavBar>
             <ContentArea>
-                    <h1 className="w-75 mx-auto mt-5">Web Portfolio</h1>
-                    <br></br>
-                    <Row xs={1} md={2} className="g-4">
-                        {Array.from({ length: 4 }).map((_, idx) => (
-                        <Col key={idx}>
-                            <Card>
-                                <Card.Img variant="top" alt="demo thumnail" src="" />
-                                <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit
-                                    longer.
-                                </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        ))}
-                    </Row>
+                <h1 className="w-75 mx-auto mt-5">Web Portfolio</h1>
+                <br></br>
+                <Row xs={1} md={2} className="g-4">
+                    {thumbs.map( data =>
+                    <Col key={data.id}>
+                        
+                        <DemoThumb
+                            title={data.name}
+                            text={data.description}
+                            img ={data.imageId}
+                            path={data.path}
+                            callBack={handleShow}
+                        ></DemoThumb>            
+                    </Col>
+                    )}
+                </Row>
+                <Modal show={show} fullscreen='true' onHide={() => setShow(false)}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>{modTitle}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Modal body content</Modal.Body>
+                </Modal>
             </ContentArea>
         </Container>
     )
