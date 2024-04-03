f9d5610381d288f4c2a6684e09c68ac03dff9b90
brodievidrine <bvidrine@sieducation.com>
2024-04-03 07:59:36 -0500

    prettier cards

--- src/components/Brokemon.js
+++ src/components/Brokemon.js
@@ -15,16 +15,16 @@ function Brokemon({
         descr,
         hp);
     return(
-        <Card className="text-center mt-2 flex-grow-1" style={{ width: '18rem', backgroundColor:'rgba(100, 0, 180, .2)', boxShadow:'2px 2px 3px #000'}}>
-            <Card.Img className="w-50 mx-auto mt-5 p-2" style={{backgroundColor:"rgba(240, 240, 240, .5)"}} variant="top" src={sprite}></Card.Img>
+        <Card className="text-center mt-2 w-100" style={{ width: '18rem', backgroundColor:'#6AD4DD', boxShadow:'2px 2px 3px #000'}}>
+            <Card.Img className="w-50 mx-auto mt-5 p-5 border border-primary" style={{backgroundColor:"#97E7E1"}} variant="top" src={sprite}></Card.Img>
             <Card.Body>
-                <Card.Title>{species}</Card.Title>
-                <Card.Text className="p-2" style={{backgroundColor:"rgba(240, 240, 240, .8)"}}>
+                <Card.Title><h3>{species}</h3></Card.Title>
+                <Card.Text className="p-2" style={{backgroundColor:"#F8F6E3"}}>
                     {descr}
                 </Card.Text>
             </Card.Body>
             <Card.Body>
-                <ListGroup className="list-group-flush">
+                <ListGroup className="list-group-flush" >
                     <ListGroup.Item> <Badge bg="success">Health:</Badge> {hp} </ListGroup.Item>
                     <ListGroup.Item> <Badge>Combat:</Badge> {totalCP} </ListGroup.Item>
                 </ListGroup>
